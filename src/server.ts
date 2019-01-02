96ab602750089b4ae70138773c907ab02e2b0ce1
ipcrm <mcadorette@gmail.com>
2019-01-02 15:08:39 -0500

    test

--- src/server.ts
+++ src/server.ts
@@ -5,43 +5,5 @@ app.get("/", (req, res) => res.send("Hello World!"));
 
 // tslint:disable-next-line:no-console
 const msg = "foo" +
-"thing" + 
-"thing" + 
-"thing" + 
-"thing" + 
-"thing" + 
-"thing" + 
-"thing" + 
-"thing" + 
-"thing" + 
-"thing" + 
-"thing" + 
-"thing" + 
-"thing" + 
-"thing" + 
-"thing" + 
-"thing" + 
-"thing" + 
-"thing" + 
-"thing" + 
-"thing" + 
-"thing" + 
-"thing" + 
-"thing" + 
-"thing" + 
-"thing" + 
-"thing" + 
-"thing" + 
-"thing" + 
-"thing" + 
-"thing" + 
-"thing" + 
-"thing" + 
-"thing" + 
-"thing" + 
-"thing" + 
-"thing" + 
-"thing" + 
-"thing" + 
 "thing";
 app.listen(3000, () => console.log("Example app listening on port 3000!"));
