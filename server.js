914e0fa075342691671499deca43c936acbc459c
ipcrm <mcadorette@gmail.com>
2019-01-02 15:11:45 -0500

    test

---
+++ server.js
@@ -0,0 +1,9 @@
+import * as express from "express";
+const app = express();
+
+app.get("/", (req, res) => res.send("Hello World!"));
+
+// tslint:disable-next-line:no-console
+const msg = "foo" +
+"thing";
+app.listen(3000, () => console.log("Example app listening on port 3000!"));
