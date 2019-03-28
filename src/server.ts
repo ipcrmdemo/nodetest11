e9cfb43a3f2a7c8b5c5e5c461bdedc8ed6d2f262
Matt Cadorette <mcadorette@gmail.com>
2019-03-28 09:36:14 -0400

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,5 @@
 import * as express from "express";
 const app = express();
 
-app.get("/", (req, res) => res.send("Hello World!"));
-app.listen(3000, () => process.stdout.write("Example app listening on port 3000!"));
+app.get("/", (req, res) => res.send("Hello World!"))
+   app.listen(3000, () => process.stdout.write("Example app listening on port 3000!"));
